c5fed3b10e6c5d381cbd5d7a6b322e86054984a5
cpython-py <zero888_666@qq.com>
2026-03-02 11:30:18 +0800

    first commit

---
+++ luffyapi/luffyapi/settings/dev.py
@@ -0,0 +1,14 @@
+'''
+Author: cpython-py zero888_666@qq.com
+Date: 2026-03-01 22:58:21
+LastEditors: cpython-py zero888_666@qq.com
+LastEditTime: 2026-03-01 23:21:41
+FilePath: \luffy\luffyapi\luffyapi\settings\dev.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from luffyapi.origin_settings import SECRET_KEY
+
+
+SECRET_KEY = 'Vlg17FXRdd_tAjon_DSU5uMn8diqXxqdM24gYzO5rf3snLOZIag--YX7NxidnET8Mmo'
+
+DEBUG = True
---
+++ luffyapi/manage.py
@@ -0,0 +1,30 @@
+'''
+Author: cpython-py zero888_666@qq.com
+Date: 2026-03-01 22:52:11
+LastEditors: cpython-py zero888_666@qq.com
+LastEditTime: 2026-03-01 23:11:07
+FilePath: \luffy\luffyapi\manage.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luffyapi.settings.dev")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
